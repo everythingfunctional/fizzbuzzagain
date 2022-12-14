c988e0917c7c99568e49df56ca6f28c6c1d48078
Brad Richardson <everythingfunctional@protonmail.com>
2022-12-14 14:40:54 -0700

    test: first fizzbuzz test

---
+++ test/fizzbuzz_test.f90
@@ -0,0 +1,26 @@
+module fizzbuzz_test
+    use fizzbuzzagain, only: fizzbuzz
+    use veggies, only: result_t, test_item_t, describe, it
+
+    implicit none
+    private
+    public :: test_fizzbuzz
+contains
+    function test_fizzbuzz() result(tests)
+        type(test_item_t) :: tests
+
+        tests = describe( &
+                "fizzbuzz", &
+                [ describe( &
+                    "for normal numbers", &
+                    [ it("returns that number as a string", check_normal_numbers) &
+                    ]) &
+                ])
+    end function
+
+    function check_normal_numbers() result(result_)
+        type(result_t) :: result_
+
+        result_ = assert_equals("1", fizzbuzz(1))
+    end function
+end module
